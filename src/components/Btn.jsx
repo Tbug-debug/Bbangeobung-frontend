feb64e65ed0653291e6445b5cc83bea8a166235c
hdlee0619 <dlgusehd1211@naver.com>
2023-02-24 22:50:22 +0900

    Design: Btn.jsx 디자인 생성

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React from 'react';
+import styled, { css } from 'styled-components';
 
-const Btn = () => {
+const Btn = ({ children, ...props }) => {
   return (
     <>
-      <BtnStyle></BtnStyle>
+      <BtnStyle {...props}>{children}</BtnStyle>
     </>
   );
 };
+
+const BtnStyle = styled.button`
+  width: 375px;
+  height: 39px;
+  border: none;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.color.btn_success};
+  font-weight: bold;
+  cursor: pointer;
+  ${(props) =>
+    props.smBtn &&
+    css`
+      width: 187px;
+      height: 39px;
+    `}
+  ${(props) =>
+    props.danger &&
+    css`
+      background-color: ${({ theme }) => theme.color.btn_danger};
+    `}
+  ${(props) =>
+    props.signUp &&
+    css`
+      background-color: ${({ theme }) => theme.color.btn_sign_up};
+      color: white;
+    `}
+  ${(props) =>
+    props.kakao &&
+    css`
+      background-color: ${({ theme }) => theme.color.btn_kakao};
+    `}
+  ${(props) =>
+    props.report &&
+    css`
+      background-color: ${({ theme }) => theme.color.btn_report};
+    `}
+`;
+
 export default Btn;
