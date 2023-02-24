9acdbb778cc7d7e943189a5d5c2f876598ac5dd5
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-02-24 22:12:10 +0900

    Docs: 파일 및 라우터 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainpages from "../components/Mainpages";
 import Login from "../pages/Login";
+import Admin from "../pages/Admin";
+import Detail from "../pages/Detail";
+import Review from "../pages/Review";
+import Signup from "../pages/Signup";
+import User from "../pages/User";
 
 function Router() {
   return (
@@ -9,6 +14,11 @@ function Router() {
       <Routes>
         <Route path="/" element={<Mainpages />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
