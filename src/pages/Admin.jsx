70ba42f3d684439e79266662338eb41de559ddf5
choi ha young <gkdudk15@naver.com>
2023-02-25 15:57:04 +0900

    Feat: 기본 틀

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div>Admin</div>
+  )
 }
 
-export default Admin;
+export default Admin
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
-function User() {
-  return <div>User</div>;
+function user() {
+    return (
+        <>
+            <UserHeader>
+                <UserImageBanner>이미지</UserImageBanner>
+                <UserInfo>
+                    <UserNicName>닉네임</UserNicName>
+                    <br></br>
+                    <UserDesc>안녕 나는 붕어빵이야</UserDesc>
+
+                    <UserImail>유저 이메일</UserImail>
+                </UserInfo>
+            </UserHeader>
+
+            <WriteList>내가 쓴목록</WriteList>
+        </>
+    );
 }
+const UserHeader = styled.div`
+    text-align: center;
+`;
 
-export default User;
+const UserImageBanner = styled.div`
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    background-color: yellow;
+`;
+
+const UserInfo = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+const UserNicName = styled.h``;
+
+const UserDesc = styled.div``;
+
+const UserImail = styled.div``;
+
+const WriteList = styled.div``;
+
+export default user;
