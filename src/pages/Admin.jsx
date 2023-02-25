85952f1329ba08515679d48939359dff96472490
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-02-25 16:42:59 +0900

    Docs: Admin.jsx, User.jsx css 완성

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Admin() {
   return (
-    <div>Admin</div>
-  )
+    <ReportBox>
+      <h1>리뷰 관련 신고</h1>
+      <ReportBoxInside></ReportBoxInside>
+      <h1>장소 관련 신고</h1>
+      <ReportBoxInside></ReportBoxInside>
+    </ReportBox>
+  );
 }
 
-export default Admin
+const ReportBox = styled.div`
+  height: 100%;
+  padding: 50px;
+`;
+
+const ReportBoxInside = styled.div`
+  border: 2px solid black;
+  height: 300px;
+`;
+
+export default Admin;
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,45 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 function user() {
-    return (
-        <>
-            <UserHeader>
-                <UserImageBanner>이미지</UserImageBanner>
-                <UserInfo>
-                    <UserNicName>닉네임</UserNicName>
-                    <br></br>
-                    <UserDesc>안녕 나는 붕어빵이야</UserDesc>
-
-                    <UserImail>유저 이메일</UserImail>
-                </UserInfo>
-            </UserHeader>
-
-            <WriteList>내가 쓴목록</WriteList>
-        </>
-    );
+  return (
+    <UserHeader>
+      <UserImageBannerBox>
+        <UserImageBanner></UserImageBanner>
+      </UserImageBannerBox>
+      <UserBox>
+        <UserInfo>
+          <UserNicName>닉네임</UserNicName>
+        </UserInfo>
+        <UserInfo>
+          <UserImail>유저 이메일</UserImail>
+        </UserInfo>
+        <UserWriteInfo>
+          <div>내가쓴 목록</div>
+        </UserWriteInfo>
+      </UserBox>
+    </UserHeader>
+  );
 }
 const UserHeader = styled.div`
-    text-align: center;
+  text-align: center;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const UserImageBannerBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 3.125rem;
 `;
 
 const UserImageBanner = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 50px;
-    background-color: yellow;
+  width: 9.375rem;
+  height: 9.375rem;
+  border-radius: 50%;
+  background-color: yellow;
+`;
+
+const UserBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const UserInfo = styled.div`
-    display: flex;
-    justify-content: center;
+  width: 25rem;
+  height: 6.25rem;
+  margin-top: 4.375rem;
+  background-color: ${({ theme }) => theme.color.item_bg};
+  border-radius: 1.25rem;
 `;
-const UserNicName = styled.h``;
 
-const UserDesc = styled.div``;
+const UserWriteInfo = styled.div`
+  width: 25rem;
+  height: 12.5rem;
+  margin-top: 4.375rem;
+  background-color: ${({ theme }) => theme.color.item_bg};
+  border-radius: 1.25rem;
+`;
+
+const UserNicName = styled.div``;
 
 const UserImail = styled.div``;
-
-const WriteList = styled.div``;
 
 export default user;
