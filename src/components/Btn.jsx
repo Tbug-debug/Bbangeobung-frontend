2b39cfa8af9aafab4de26542f1f5b26b4fa0707c
hdlee0619 <dlgusehd1211@naver.com>
2023-02-25 15:24:54 +0900

    Style: console.log 삭제

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import styled, { css } from "styled-components";
+import React from 'react';
+import styled, { css } from 'styled-components';
 
 const Btn = ({ children, ...props }) => {
-  console.log(props);
   return (
     <>
       <BtnStyle {...props}>{children}</BtnStyle>
