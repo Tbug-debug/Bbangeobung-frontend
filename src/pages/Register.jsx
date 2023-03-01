77674f77e8dfe30b21606daa1a8301996cc82c98
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-03-02 01:25:49 +0900

    Docs: 이미지 미리보기 기능 추가

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -27,6 +27,7 @@ function Register() {
   const [long, setLong] = useState("");
   const [lati, setLati] = useState("");
   const [body, setBody] = useState("");
+  const [preview, setPreview] = useState("");
   const [formImagin, setFormformImagin] = useState(new FormData());
   const queryClient = useQueryClient();
 
@@ -96,7 +97,17 @@ function Register() {
     const img = e.target.files[0];
     const formImg = new FormData();
     formImg.append("imageFile", img);
+    const reader = new FileReader();
     setFormformImagin(formImg);
+    reader.onloadend = () => {
+      setPreview(reader.result);
+    };
+
+    if (img) {
+      reader.readAsDataURL(img);
+    } else {
+      setPreview("");
+    }
   }
 
   function handleSubmit() {
@@ -144,6 +155,18 @@ function Register() {
             <RegisterRegistorTitle>
               &lt;붕어빵 사진 아니면 IP 벤 ㅅㄱ&gt;
             </RegisterRegistorTitle>
+            {preview && (
+              <img
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  borderRadius: "20px",
+                  marginBottom: "40px",
+                }}
+                src={preview}
+                alt="Preview"
+              />
+            )}
             <FileInput type="file" accept="image/*" onChange={onChangeimge} />
             <RegisterInputTitle>&lt;붕어빵 위치 좌표! &gt;</RegisterInputTitle>
             <PositonInput
